d8464755865120551e8a40e583defa86c6b1b940
Alexx <eralexxnoyi@gmail.com>
2018-04-29 20:17:14 -0600

    Mantenimiento tabla turno con jstl y beans, agregue pull de conexion y añadi los archivos properties para la internacionalización

---
+++ Proyecto_Java/src/java/Beans/TurnoBeans.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+/**
+ *
+ * @author Alexx
+ */
+public class TurnoBeans {
+    private String cmb_turno;
+    private String txt_turno;
+    private String txt_hora_inicio;
+    private String txt_hora_fin;
+    
+    /**
+     * @return the cmb_turno
+     */
+    public String getCmb_turno() {
+        return cmb_turno;
+    }
+
+    /**
+     * @param cmb_turno the cmb_turno to set
+     */
+    public void setCmb_turno(String cmb_turno) {
+        this.cmb_turno = cmb_turno;
+    }
+
+    /**
+     * @return the txt_turno
+     */
+    public String getTxt_turno() {
+        return txt_turno;
+    }
+
+    /**
+     * @param txt_turno the txt_turno to set
+     */
+    public void setTxt_turno(String txt_turno) {
+        this.txt_turno = txt_turno;
+    }
+
+    /**
+     * @return the txt_hora_inicio
+     */
+    public String getTxt_hora_inicio() {
+        return txt_hora_inicio;
+    }
+
+    /**
+     * @param txt_hora_inicio the txt_hora_inicio to set
+     */
+    public void setTxt_hora_inicio(String txt_hora_inicio) {
+        this.txt_hora_inicio = txt_hora_inicio;
+    }
+
+    /**
+     * @return the txt_hora_fin
+     */
+    public String getTxt_hora_fin() {
+        return txt_hora_fin;
+    }
+
+    /**
+     * @param txt_hora_fin the txt_hora_fin to set
+     */
+    public void setTxt_hora_fin(String txt_hora_fin) {
+        this.txt_hora_fin = txt_hora_fin;
+    }
+}
