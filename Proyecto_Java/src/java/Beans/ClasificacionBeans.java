1696e8187700b450bb3741977211cdf63d34d79e
Karina Marroquin <karina24magaly@gmail.com>
2018-05-10 22:39:33 -0600

    Update ClasificacionBeans.java

--- Proyecto_Java/src/java/Beans/ClasificacionBeans.java
+++ Proyecto_Java/src/java/Beans/ClasificacionBeans.java
@@ -7,11 +7,40 @@ package Beans;
 
 /**
  *
- * @author Alexx
+ * @author karin
  */
-public class ClasificacionBeans {
-    private String cmb_cargo;
-    private String txt_cargo;
-    private String txt_descripcion;
+public class AutorBeans {
+
+    private String cmb_autor;
+    private String txt_autor;
+    
+    /**
+     * @return the cmb_autor
+     */
+    public String getCmb_autor() {
+        return cmb_autor;
+    }
+
+    /**
+     * @param cmb_autor the cmb_autor to set
+     */
+    public void setCmb_autor(String cmb_autor) {
+        this.cmb_autor = cmb_autor;
+    }
+
+    /**
+     * @return the txt_autor
+     */
+    public String getTxt_autor() {
+        return txt_autor;
+    }
+
+    /**
+     * @param txt_autor the txt_autor to set
+     */
+    public void setTxt_autor(String txt_autor) {
+        this.txt_autor = txt_autor;
+    }
     
 }
+
